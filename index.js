210d09d5b48f692bf11a3c0870886d12be1d6349
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-07-21 14:40:59 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  const data = {
+    name: name,
+    email: email
+  };
+
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    const idElement = document.createElement('P');
+    idElement.textContent = data.id;
+    document.body.appendChild(idElement)
+  })
+  .catch(error => {
+    const errorElement = document.createElement('P');
+    errorElement.textContent = error.message;
+    document.body.appendChild(errorElement)
+  });
+}
